f313a3c37cd973dac208af31634138600b14cf0c
Amanda <amanda.barboza@objective.com.br>
2021-08-26 15:36:47 -0300

    add all functionality asked

--- 01/Multiple.js
+++ 01/Multiple.js
@@ -1,18 +1,53 @@
 class Multiple {
-	checkMultiple (value, multiple) {
+	isMultiple(value, multiple) {
 		return value % multiple === 0;
 	}
 
-	sum(limit, multiples) {
-		let multiple = 0
+	isMultipleOfNumber(value, multiples) {
+		return multiples.some((multiple) => value % multiple === 0);
+	}
+
+	isMultipleOfAllMultiples(value, multiples) {
+		return multiples.every((multiple) => value % multiple === 0);
+	}
+
+	sumOfMultipleOfNumber(limit, multiples) {
+		let multiple = 0;
 
 		for (let value = 0; value < limit; value++) {
-			if(this.checkMultiple(value, multiples[0]) || this.checkMultiple(value, multiples[1])) {
-				multiple += value
+			if (this.isMultipleOfNumber(value, multiples)) {
+				multiple += value;
 			}
 		}
-		
-		return multiple
+
+		return multiple;
+	}
+
+	sumOfMultipleOfMultiples(limit, multiples) {
+		let multiple = 0;
+
+		for (let value = 0; value < limit; value++) {
+			if (this.isMultipleOfAllMultiples(value, multiples)) {
+				multiple += value;
+			}
+		}
+
+		return multiple;
+	}
+
+	sumOfMultipleOfMultiplesAndMultiple(limit, multiples, multiples2) {
+		let multiple = 0;
+
+		for (let value = 0; value < limit; value++) {
+			if (
+				this.isMultipleOfNumber(value, multiples) &&
+				this.isMultipleOfAllMultiples(value, multiples2)
+			) {
+				multiple += value;
+			}
+		}
+
+		return multiple;
 	}
 }
 
--- 01/index.test.js
+++ 01/index.test.js
@@ -1,43 +1,86 @@
 const Multiple = require("./Multiple");
 
 describe("check if number is multiple of 3 or 5", () => {
+
 	test("if the number is multiple of 3", () => {
 		const expectedValue = true;
-		const multiple = 3
+		const multiple = 3;
 		const checkNumber = new Multiple();
 
-		const result = checkNumber.checkMultiple(3, multiple);
+		const result = checkNumber.isMultiple(3, multiple);
 
 		expect(expectedValue).toEqual(result);
 	});
 
 	test("if the number is multiple of 5", () => {
 		const expectedValue = true;
-		const multiple = 5
+		const multiple = 5;
 		const checkNumber = new Multiple();
 
-		const result = checkNumber.checkMultiple(10, multiple);
+		const result = checkNumber.isMultiple(10, multiple);
 
 		expect(expectedValue).toEqual(result);
 	});
 
 	test("if the number is multiple of 7", () => {
 		const expectedValue = true;
-		const multiple = 7
+		const multiple = 7;
 		const checkNumber = new Multiple();
 
-		const result = checkNumber.checkMultiple(7, multiple);
+		const result = checkNumber.isMultiple(7, multiple);
+
+		expect(expectedValue).toEqual(result);
+	});
+
+	test("if the number is multiple of one of multiples numbers", () => {
+		const expectedValue = true;
+		const multiples = [3, 5];
+		const checkNumber = new Multiple();
+
+		const result = checkNumber.isMultipleOfNumber(5, multiples);
+
+		expect(expectedValue).toEqual(result);
+	});
+
+	test("if the number is multiple of all multiples numbers", () => {
+		const expectedValue = true;
+		const multiples = [3, 5];
+		const checkNumber = new Multiple();
+
+		const result = checkNumber.isMultipleOfAllMultiples(15, multiples);
 
 		expect(expectedValue).toEqual(result);
 	});
 
 	test("if the sum of numbers multiple of 3 or 5 below 10 is 23", () => {
 		const expectedValue = 23;
+		const multiples = [3, 5];
 		const checkNumber = new Multiple();
 
-		const result = checkNumber.sum(10, [3, 5]);
+		const result = checkNumber.sumOfMultipleOfNumber(10, multiples);
 
 		expect(expectedValue).toEqual(result);
 	});
 
+	test("if the sum of numbers multiple of 3 and 5 below 10 is 15", () => {
+		const expectedValue = 15;
+		const multiples = [3, 5];
+		const checkNumber = new Multiple();
+
+		const result = checkNumber.sumOfMultipleOfMultiples(16, multiples);
+
+		expect(expectedValue).toEqual(result);
+	});
+
+	test("sum all natural numbers multiple of (3 or 5) and 7", () => {
+		const expectedValue = 56;
+		const multiples = [3, 5];
+		const multiples2 = [7];
+		const checkNumber = new Multiple();
+
+		const result = checkNumber.sumOfMultipleOfMultiplesAndMultiple(36, multiples, multiples2);
+
+		expect(expectedValue).toEqual(result);
+	})
+	
 });
